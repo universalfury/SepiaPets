f5e038d47234c0d765290dbdb8e44cc44a6e1a02
Vartika Singh <vartika.singh@tothenew.com>
2022-11-28 19:50:16 +0530

    [WIP]: Initialized an empty project file

---
+++ SepiaSetsUITests/SepiaSetsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SepiaSetsUITestsLaunchTests.swift
+//  SepiaSetsUITests
+//
+//  Created by Vartika Singh on 28/11/22.
+//
+
+import XCTest
+
+class SepiaSetsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
